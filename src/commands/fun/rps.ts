d3891f543cbb75ed3c1ed8f57598396c449d63fa
Xylight <james@xylight.us>
2023-06-01 17:08:41 -0700

    feat: Add opponent to RPS command

--- src/commands/fun/rps.ts
+++ src/commands/fun/rps.ts
@@ -5,11 +5,18 @@ import {
     ApplicationCommandOptionType,
     ButtonBuilder,
     ButtonStyle,
+    ComponentType,
     EmbedBuilder,
 } from 'discord.js'
 import { asDisabled } from 'util/component.js'
 
 type RPSChoice = 'rock' | 'paper' | 'scissors'
+
+enum RPSEmoji {
+    rock = '🪨',
+    paper = '📜',
+    scissors = '✂️',
+}
 
 function getRandomChoice(): RPSChoice {
     const choices: RPSChoice[] = ['rock', 'paper', 'scissors']
@@ -33,10 +40,21 @@ export default {
     metadata: {
         type: ApplicationCommandOptionType.Subcommand,
         name: 'rockpaperscissors',
-        description: 'Play a rock paper scissors match with the bot',
+        description: 'Play a rock paper scissors match with someone or the bot',
+        options: [
+            {
+                name: 'opponent',
+                type: ApplicationCommandOptionType.User,
+                description:
+                    'Who you want to play with. If empty, will play with the bot.',
+                required: false,
+            },
+        ],
     },
 
-    async execute({ interaction }) {
+    async execute({ interaction, client }) {
+        const opponent = interaction.options.getUser('opponent')
+
         const gameButtons = new ActionRowBuilder<ButtonBuilder>().setComponents(
             new ButtonBuilder({
                 customId: 'xylo:fun:rps:rock',
@@ -83,69 +101,122 @@ export default {
         while (playing) {
             row = gameButtons
 
-            if (!(playerScore == 0 && opponentScore == 0)) {
-                embed.setFields(
-                    {
-                        name: 'You',
-                        value: `${playerScore}`,
-                        inline: true,
-                    },
-                    {
-                        name: 'Opponent',
-                        value: `${opponentScore}`,
-                        inline: true,
-                    }
-                )
-            }
+            embed.setFields(
+                {
+                    name: `${interaction.user.username}`,
+                    value: `${playerScore}`,
+                    inline: true,
+                },
+                {
+                    name: `${opponent?.username ?? client.user?.username}`,
+                    value: `${opponentScore}`,
+                    inline: true,
+                }
+            )
 
-            await interaction.editReply({
+            const reply = await interaction.editReply({
                 embeds: [embed],
                 components: [row],
             })
 
-            const buttonInt = await reply
-                .awaitMessageComponent({
-                    filter: (int) => int.user == interaction.user,
-                    time: 60 * 1000,
-                    dispose: true,
-                })
-                .catch((err) => {
-                    playing = false
-                })
+            let playerChoice: RPSChoice | undefined
+            let opponentChoice: RPSChoice | undefined
 
-            if (!playing || !buttonInt) break
+            if (!opponent) {
+                opponentChoice = getRandomChoice()
+            }
 
-            buttonInt.deferUpdate()
+            let waiting = true
+            while (waiting) {
+                const int = await reply
+                    .awaitMessageComponent({
+                        time: 30 * 1000,
+                        dispose: true,
+                        filter: (int) =>
+                            int.user.id == interaction.user.id ||
+                            int.user.id == opponent?.id,
+                        componentType: ComponentType.Button,
+                    })
+                    .catch((_) => {
+                        waiting = false
+                    })
 
-            let choice: RPSChoice = buttonInt.customId
-                .split(':')
-                .at(-1) as RPSChoice
+                if (!waiting || !int) break
 
-            let opponentChoice = getRandomChoice()
+                int.deferUpdate()
+
+                const choice = int.customId.split(':').at(-1) as RPSChoice
+
+                if (int.user.id == interaction.user.id) playerChoice = choice
+                else if (int.user.id == opponent?.id) opponentChoice = choice
+
+                if (playerChoice && opponentChoice) break
+
+                if (opponent) {
+                    await interaction.editReply({
+                        embeds: [
+                            new EmbedBuilder({
+                                title: 'Rock paper scissors',
+                                description: `Waiting for ${
+                                    playerChoice && !opponentChoice
+                                        ? `<@${opponent.id}>`
+                                        : `<@${interaction.user.id}>`
+                                }`,
+                                color: Color.primary,
+                                fields: [
+                                    {
+                                        name: `${interaction.user.username}`,
+                                        value: `${playerScore}`,
+                                        inline: true,
+                                    },
+                                    {
+                                        name: `${
+                                            opponent?.username ??
+                                            client.user?.username
+                                        }`,
+                                        value: `${opponentScore}`,
+                                        inline: true,
+                                    },
+                                ],
+                            }),
+                        ],
+                    })
+                }
+            }
+
+            if (!playing || !(playerChoice && opponentChoice)) break
 
             const resultsEmbed = new EmbedBuilder({
                 title: 'Rock paper scissors',
                 fields: [
                     {
-                        name: 'Your choice',
-                        value: capitalizeFirstLetter(choice),
+                        name: `${interaction.user.username}`,
+                        value: `${RPSEmoji[playerChoice]}`,
+                        inline: true,
                     },
                     {
-                        name: 'Opponent choice',
-                        value: capitalizeFirstLetter(opponentChoice),
+                        name: `${opponent?.username ?? client.user?.username}`,
+                        value: `${RPSEmoji[opponentChoice]}`,
+                        inline: true,
                     },
                 ],
                 color: Color.primary,
             })
 
-            if (beats(choice, opponentChoice)) {
-                resultsEmbed.setTitle('**You win! :partying_face:**')
+            if (beats(playerChoice, opponentChoice)) {
+                resultsEmbed.setDescription(
+                    `**${interaction.user.username}** wins! :partying_face:`
+                )
                 playerScore += 1
-            } else if (beats(opponentChoice, choice)) {
-                resultsEmbed.setTitle('**You lose :confused:**')
+            } else if (beats(opponentChoice, playerChoice)) {
+                resultsEmbed.setDescription(
+                    `**${
+                        opponent?.username ?? client.user?.username
+                    }** wins! :partying_face:`
+                )
                 opponentScore += 1
             } else {
-                resultsEmbed.setTitle("**It's a tie! :no_mouth:**")
+                resultsEmbed.setDescription("It's a **tie**! :no_mouth:")
             }
 
             row = rematch
@@ -157,7 +228,9 @@ export default {
 
             const rematchInt = await reply
                 .awaitMessageComponent({
-                    filter: (int) => int.user == interaction.user,
+                    filter: (int) =>
+                        int.user.id == interaction.user.id ||
+                        int.user.id == opponent?.id,
                     time: 60 * 1000,
                     dispose: true,
                 })
